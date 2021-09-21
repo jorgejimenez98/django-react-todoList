e2ce20d0ce629f8e5c92f176ec21c20bfec30f2c
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-09-21 12:11:27 +0200

    ACTUALIZADO enrutamiento en el app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,39 @@
 import "./App.css";
 import Header from "./components/Header";
 import Casa from "./components/Casa";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Comentarios from "./components/Comentarios";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+const routes = [
+  { path: "/todos", component: Casa, exact: true },
+  { path: "/todos/:todoId/comentarios", component: Comentarios, exact: false },
+];
 
 function App() {
   return (
     <Router>
       <Header />
       <main className="py-3">
+        <Switch>
+          {routes.map((route, idx) => {
+            return (
+              route.component && (
+                <Route
+                  key={idx}
+                  path={route.path}
+                  exact={route.exact}
+                  render={(props) => <route.component {...props} />}
+                />
+              )
+            );
+          })}
+          <Redirect from="/" to="/todos" />
+        </Switch>
         <Route path="/" component={Casa} exact />
       </main>
     </Router>
